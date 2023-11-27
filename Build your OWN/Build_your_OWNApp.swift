63aa10b33b5998a40397657594a4f20eb18ec082
chris robles <croblesquezad@bsd220.org>
2023-11-27 09:50:38 -0600

    Initial Commit

---
+++ Build your OWN/Build_your_OWNApp.swift
@@ -0,0 +1,17 @@
+//
+//  Build_your_OWNApp.swift
+//  Build your OWN
+//
+//  Created by christopher robles quezada on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Build_your_OWNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

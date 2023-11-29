3dcaaa31efa0e6db53d9e5b2ecabb58bf7ec8e7f
chris robles <croblesquezad@bsd220.org>
2023-11-29 09:49:53 -0600

    added Treasure chest dice title.

--- Build your OWN/ContentView.swift
+++ Build your OWN/ContentView.swift
@@ -8,17 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+@State private var gameScore = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Treasure Dice")
+                .font(.title)
+                .bold()
+                Spacer()
+            Image("treasurechest").resizable().frame(width: 245, height: 245)
+            
         }
         .padding()
     }
 }
 
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text)
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()

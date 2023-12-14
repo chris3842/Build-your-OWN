4a996c45723b2aca6d7ea3b1072e621361058065
chris robles <croblesquezad@bsd220.org>
2023-12-14 09:55:39 -0600

    "added the roll button "

--- Build your OWN/ContentView.swift
+++ Build your OWN/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var randomValue = 0
     @State private var rotation = 0.0
     var body: some View {
-        NavigationView() {
+        NavigationView {
             VStack {
                 Text("Treasure Dice")
                     .font(.title)
@@ -31,12 +31,12 @@ struct ContentView: View {
                         }
                     }
                 }
+                NavigationLink("How To Play", destination: InstructionsView())
+                    .font(Font.custom("Marker Felt",size: 24))
+                    .padding()
+                Spacer()
             }
         }
-        NavigationLink("How To Play", destination: InstructionsView())
-            .font(Font.custom("Marker Felt",size: 24))
-            .padding()
-            Spacer()
     }
     
     func chooseRandom(times:Int) {
@@ -46,48 +46,49 @@ struct ContentView: View {
             }
         }
     }
-    
-    struct CustomText: View {
-        let text: String
-        var body: some View {
-            Text(text)
-        }
+}
+
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text)
     }
-    
-    struct CustomButtomStyle:ButtonStyle {
-        func makeBody(configuration: Configuration) -> some View {
-            configuration.label
-                .frame(width: 50)
-                .font(Font.custom("marker felt", size: 24))
-                .padding()
-                .background(.black).opacity(configuration.isPressed ? 0.0 : 1.0)
-                .foregroundColor(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-        }
+}
+
+struct CustomButtomStyle:ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 50)
+            .font(Font.custom("marker felt", size: 24))
+            .padding()
+            .background(.black).opacity(configuration.isPressed ? 0.0 : 1.0)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
-    
-    struct InstructionsView: View {
-        var body: some View {
-            VStack {
-                Image("dust 1").resizable().frame(width: 150, height:  150)
-                Text("Treasure Dice game").font(.title)
-                VStack(alignment: .leading){
-                    Text("in Treasure Dice you need get threechests to win the game. if you dont get three chests and get three dust u lose the game.")
-                        .padding()
-                    Text("first you press the roll button then it rolls and what ever u get thats what u have as one for right now.")
-                        .padding()
-                    Text("after getting three dust u will lose and get no gold and you dont win the game")
-                        .padding()
-                    Text("if you get three chests you will win the game and restart the game to play again")
-                    
-                }
+}
+
+struct InstructionsView: View {
+    var body: some View {
+        VStack {
+            Image("dust 1").resizable().frame(width: 150, height:  150)
+            Text("Treasure Dice game").font(.title)
+            VStack(alignment: .leading){
+                Text("in Treasure Dice you need get threechests to win the game. if you dont get three chests and get three dust u lose the game.")
+                    .padding()
+                Text("first you press the roll button then it rolls and what ever u get thats what u have as one for right now.")
+                    .padding()
+                Text("after getting three dust u will lose and get no gold and you dont win the game")
+                    .padding()
+                Text("if you get three chests you will win the game and restart the game to play again")
+                
             }
+            Spacer()
         }
     }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }

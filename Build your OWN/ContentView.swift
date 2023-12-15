05fd3b50d3b0c9b2ff2f204ed1aa478ec27fbfb3
chris robles <croblesquezad@bsd220.org>
2023-12-15 09:24:52 -0600

    "winning screen"

--- Build your OWN/ContentView.swift
+++ Build your OWN/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var gameScore = 0
     @State private var randomValue = 0
     @State private var rotation = 0.0
+    @State private var gameWon = false
     var body: some View {
         NavigationView {
             VStack {
@@ -29,12 +30,33 @@ struct ContentView: View {
                         withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
                             rotation += 360
                         }
+                        if gameWon {
+                            gameScore = 0
+                            gameWon = false
+                    
+                        } else {
+                                
+                            }
+                        }
                     }
                 }
+                
                 NavigationLink("How To Play", destination: InstructionsView())
                     .font(Font.custom("Marker Felt",size: 24))
                     .padding()
                 Spacer()
+            }
+            .sheet(isPresented: $gameWon, content: {
+                WinningView()
+            })
+        }
+
+    struct WinningView: View {
+        var body: some View {
+            VStack {
+                Text("congratulations! You won")
+                    .font(.title)
+                    .bold()
             }
         }
     }
@@ -43,52 +65,61 @@ struct ContentView: View {
         if times > 0 {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 randomValue = Int.random(in: 1...5)
+                checkGameResult()
             }
+        }
+    }
+    
+    func checkGameResult() {
+        if randomValue == 3 {
+            gameWon = true
+        } else if gameScore >= 3 {
+        }
+    }
+    
+    struct CustomText: View {
+        let text: String
+        var body: some View {
+            Text(text)
+        }
+    }
+    
+    struct CustomButtomStyle:ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .frame(width: 50)
+                .font(Font.custom("marker felt", size: 24))
+                .padding()
+                .background(.black).opacity(configuration.isPressed ? 0.0 : 1.0)
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
+    
+    struct InstructionsView: View {
+        var body: some View {
+            VStack {
+                Image("dust 1").resizable().frame(width: 150, height:  150)
+                Text("Treasure Dice game").font(.title)
+                VStack(alignment: .leading){
+                    Text("in Treasure Dice you need get threechests to win the game. if you dont get three chests and get three dust u lose the game.")
+                        .padding()
+                    Text("first you press the roll button then it rolls and what ever u get thats what u have as one for right now.")
+                        .padding()
+                    Text("after getting three dust u will lose and get no gold and you dont win the game")
+                        .padding()
+                    Text("if you get three chests you will win the game and restart the game to play again")
+                    
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
     }
 }
 
-struct CustomText: View {
-    let text: String
-    var body: some View {
-        Text(text)
-    }
-}
-
-struct CustomButtomStyle:ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .frame(width: 50)
-            .font(Font.custom("marker felt", size: 24))
-            .padding()
-            .background(.black).opacity(configuration.isPressed ? 0.0 : 1.0)
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-struct InstructionsView: View {
-    var body: some View {
-        VStack {
-            Image("dust 1").resizable().frame(width: 150, height:  150)
-            Text("Treasure Dice game").font(.title)
-            VStack(alignment: .leading){
-                Text("in Treasure Dice you need get threechests to win the game. if you dont get three chests and get three dust u lose the game.")
-                    .padding()
-                Text("first you press the roll button then it rolls and what ever u get thats what u have as one for right now.")
-                    .padding()
-                Text("after getting three dust u will lose and get no gold and you dont win the game")
-                    .padding()
-                Text("if you get three chests you will win the game and restart the game to play again")
-                
-            }
-            Spacer()
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
